132d855a113605a6f2d7347073ec574931984ccd
omelhoro <igor.fischer154@gmail.com>
2015-11-03 22:27:19 +0100

    formating

--- web/voclib/voc_task.dart
+++ web/voclib/voc_task.dart
@@ -11,34 +11,36 @@ class Task extends PolymerElement {
 
   checkMulti(Event evt) {
     TableCellElement t = evt.target;
-    t.dataset["sol"] == "true" ? t.classes.add("success") : t.classes.add("danger");
+    t.dataset["sol"] == "true"
+        ? t.classes.add("success")
+        : t.classes.add("danger");
   }
 
   checkSingle(Event evt) {
     InputElement t = evt.target;
     if (t.dataset["sol"] == t.value) {
       t.parent.classes
-          ..remove("has-error")
-          ..add("has-success");
+        ..remove("has-error")
+        ..add("has-success");
     } else {
       t.parent.classes
-          ..remove("has-success")
-          ..add("has-error");
+        ..remove("has-success")
+        ..add("has-error");
     }
   }
-
 
   reset(List<String> l) {
     InputElement el = shadowRoot.querySelector(".singleinput");
     el.value = "";
     el.classes
-        ..remove("right")
-        ..remove("false"); //has a dummy argument for updating when alternatives got updated
+      ..remove("right")
+      ..remove(
+          "false"); //has a dummy argument for updating when alternatives got updated
   }
+
   switchView() {
     isSingleView = !isSingleView;
   }
-  checkall() {
-  }
 
+  checkall() {}
 }
